904de86e2dd951ea00e31bf08af4cb1c428a1c7f
Sandeep Purohit <sandeep@Sandeeps-MacBook-Pro.local>
2017-05-17 12:48:16 +0200

    add akka logging

--- build.sbt
+++ build.sbt
@@ -8,6 +8,8 @@ libraryDependencies ++= Seq(
 	"com.typesafe.akka" % "akka-actor_2.11" % "2.4.12",
 	"com.typesafe.akka" % "akka-http_2.11" % "3.0.0-RC1",
 	"com.typesafe.akka" % "akka-http-core_2.11" % "3.0.0-RC1",
-	"com.typesafe.akka" % "akka-http-spray-json_2.11" % "3.0.0-RC1"
+	"com.typesafe.akka" % "akka-http-spray-json_2.11" % "3.0.0-RC1",
+	"com.typesafe.akka" %% "akka-slf4j" % "2.4.12",
+	"ch.qos.logback" % "logback-classic" % "1.1.7"
 )
 
--- src/main/scala/com/paratha/AkkaSimple.scala
+++ src/main/scala/com/paratha/AkkaSimple.scala
@@ -1,13 +1,14 @@
 package com.paratha
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.event.Logging.LoggerInitialized
 import akka.pattern.ask
 import akka.util.Timeout
-import scala.util.Success
-import scala.concurrent.duration._
-import scala.concurrent.Future
+
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Failure
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+import akka.event.Logging
 
 /**
   * Created by sandeep on 15/05/2017.
@@ -15,23 +16,28 @@ import scala.util.Failure
 object AkkaSimple extends App {
 
   val system = ActorSystem("system")
+  val log=Logging(system,classOf[MyActor])
+  log.debug("startttttttttt")
   val myActor = system.actorOf(Props[MyActor], "myActor")
   val askActor = system.actorOf(Props[AskActor], "askActor")
-  implicit val timeout=Timeout(1 seconds)
+  implicit val timeout = Timeout(1 seconds)
 
   myActor ! "its"
   myActor ! 2
   myActor ! "fdsf"
   val reply = (askActor ? "i have query").mapTo[String]
   reply onComplete {
-    case Success(n)=>println(n)
-    case Failure(e)=>println("Failure")
+    case Success(n) => println(n)
+    case Failure(e) => println("Failure")
   }
 }
 
-class MyActor extends Actor {
+class MyActor extends Actor with ActorLogging {
+
   override def receive: Receive = {
-    case _: String => println("its string")
+    case _: String =>
+      log.debug("get string message")
+      println("its string")
     case _: Int => println("its number")
     case _ => println("something else")
   }
